ac104e3731eb7555abdaa0a9054d75d59d2b1cf1
Tima <tima52rus@gmail.com>
2022-07-03 20:49:40 +0300

    update, and fixed bugs

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,7 +6,6 @@ export class Service {
 	timer$ = new BehaviorSubject(this.initialTime);
 	lastTime = this.initialTime;
 	isRunning = false;
-	intervalSec = 0;
 	timerSubscription = new Subscription();
 
 	get StopWatch$(): Observable<ITime> {
@@ -20,7 +19,6 @@ export class Service {
 		this.timerSubscription = timer(0, 1000)
 			.pipe(
 				map((v: number) => {
-					console.log(v);
 					return v + this.lastTime;
 				})
 			)
@@ -38,14 +36,13 @@ export class Service {
 		this.timerSubscription.unsubscribe();
 		this.lastTime = this.initialTime;
 		this.timer$.next(this.initialTime);
+		this.isRunning = false;
 	}
 
 	secondsToStopWatch(sec: number): ITime {
-		let rest = sec % 1;
-		if (sec % 6 === 0) this.intervalSec = Math.floor(sec / 60);
-		const seconds = this.intervalSec % 6;
-		rest = sec % 3600;
-		const minutes = Math.floor(rest / 60);
+		let targetSec = sec % 60;
+		const seconds = targetSec;
+		const minutes = Math.floor(sec / 60);
 
 		return {
 			minutes: this.timeToString(minutes),
